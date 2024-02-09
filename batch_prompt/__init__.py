c05e896a57bb7e500707c33a831edfeecdd99f79
Eric Bigelow <8014461+ebigelow@users.noreply.github.com>
2024-02-09 17:11:44 -0500

    add __init__.

---
+++ batch_prompt/__init__.py
@@ -0,0 +1,3 @@
+from batch_prompt.completions import get_completions as completions
+from batch_prompt.chat_completions import get_chat_completions as chat_completions
+from batch_prompt.utils import openai
